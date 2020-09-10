3543c9e32195af2f1df81eea3fef1ab44fea6fcb
Sumit Kumar <sumit09211@gmail.com>
2020-09-10 12:16:13 +0530

    fix : hourly, fullday dyanmic values and multiple fixes

--- src/ui-molecules-local/BookingTimeSlot/index.js
+++ src/ui-molecules-local/BookingTimeSlot/index.js
@@ -10,6 +10,10 @@ import TableBody from "@material-ui/core/TableBody";
 import { withStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import get from "lodash/get";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import Grid from "@material-ui/core/Grid";
+
 import {
     prepareFinalObject,
     toggleSnackbar,
@@ -19,6 +23,31 @@ import {
 class CustomTimeSlots extends Component {
     constructor() {
         super();
+        let currentDateObj = new Date();
+        let month = ('0' + (currentDateObj.getMonth() + 1)).slice(-2);
+        let day = ('0' + currentDateObj.getDate()).slice(-2);
+        let year = currentDateObj.getFullYear();
+        let currentDate = `${year}-${month}-${day}`;
+        let currentDateForLimit = new Date();
+        currentDateForLimit.setDate(currentDateForLimit.getDate() + 179);
+        let limitMonth = ('0' + (currentDateForLimit.getMonth() + 1)).slice(-2);
+        let limitDay = ('0' + currentDateForLimit.getDate()).slice(-2);
+        let limitYear = currentDateForLimit.getFullYear();
+        let currentLimitDate = `${limitYear}-${limitMonth}-${limitDay}`;
+
+        let dateInputProps = {
+            min: currentDate,
+            max: currentLimitDate
+        }
+
+        this.state = {
+
+            currentSlide: 0,
+            selectedDate: '',
+            currentDate: currentDate,
+            dateInputProps: dateInputProps
+
+        }
         //     this.state = {
         //         reservedTimeSlotsDate: '',
         //     }
@@ -117,6 +146,35 @@ class CustomTimeSlots extends Component {
         //)
 
     }
+
+    goHandler = () => {
+
+        var [goYear, goMonth, goDay] = this.state.selectedDate.split("-");
+        let goDate = `${goDay}-${goMonth}-${goYear}`
+        let i = 0;
+
+        for (i; i < this.props.rows.length; i++) {
+
+            if (this.props.rows[i].date == goDate) {
+                break;
+            }
+        }
+        console.log(i, 'i value');
+        this.setState((state) => ({
+            currentSlide: i,
+        }));
+
+    }
+    updateCurrentSlide = (index) => {
+
+        const { currentSlide } = this.state;
+
+        if (currentSlide !== index) {
+            this.setState({
+                currentSlide: index,
+            });
+        }
+    };
     render() {
 
         const classes = withStyles();
@@ -135,116 +193,155 @@ class CustomTimeSlots extends Component {
 
 
         return (
-            <Carousel
-                renderArrowPrev={(onClickHandler, hasPrev, label) =>
-                    hasPrev && (
-                        <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
-                            {"<"}
-                        </button>
-                    )
-                }
-                renderArrowNext={(onClickHandler, hasNext, label) =>
-                    hasNext && (
-                        <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, right: 15 }}>
-                            {">"}
-                        </button>
-                    )
-                }
-            >
+            <div>
+                <Grid container={true} justify="flex-end">
+                    <Grid item={true} xs={2}>
+                        <TextField
+                            id="date"
+                            label="Jump Date"
+                            type="date"
+                            defaultValue={this.state.currentDate}
+                            onChange={(event) => {
 
-                {rows && rows.map((item) => {
-                    return <div>
-                        <Paper className={classes.root}>
-                            <Table className={classes.table}>
-                                <TableHead className={`timeslot-table-head ${classes.head}`}>
+                                this.setState({ selectedDate: event.target.value })
+                            }}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
 
-                                    <TableRow >
+                            inputProps={this.state.dateInputProps}
+                            style={{ marginLeft: "50px"}}
 
-                                        <TableCell className={"header-date"} colSpan={3} align={"center"}>
-                                            {item.date}
-                                        </TableCell>
+                        />
 
-                                    </TableRow>
+                    </Grid>
+                    <Grid item={true} xs={2}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={this.goHandler}
+                            style={{
+                                minWidth: "100px",
+                                height: "40px",
+                                marginTop: "15px",
+                                float: "right"
+                            }} >
+                            GO
+                </Button>
+                    </Grid>
+                </Grid>
+                <Carousel
+                    selectedItem={this.state.currentSlide}
+                    onChange={this.updateCurrentSlide}
+                    renderArrowPrev={(onClickHandler, hasPrev, label) =>
+                        hasPrev && (
+                            <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
+                                {"<"}
+                            </button>
+                        )
+                    }
+                    renderArrowNext={(onClickHandler, hasNext, label) =>
+                        hasNext && (
+                            <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, right: 15 }}>
+                                {">"}
+                            </button>
+                        )
+                    }
+                >
 
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        {item.timeSlots.map((item1, id) => {
-                                            let availabilityClass = '';
-                                            let timeSlotExpired = ''
-                                            if (item1.indexOf(":booked") !== -1) {
-                                                item1 = item1.split(":")[0];
-                                                availabilityClass = "booked-time-slot"
-                                            } else {
-                                                availabilityClass = "available-time-slot"
-                                            }
+                    {rows && rows.map((item) => {
+                        return <div>
+                            <Paper className={classes.root}>
+                                <Table className={classes.table}>
+                                    <TableHead className={`timeslot-table-head ${classes.head}`}>
 
-                                            //let currentDate = this.state.currentDate;
-                                            let currentTime = parseFloat(`${currentDate.getHours()}.${currentDate.getMinutes()}`);
+                                        <TableRow >
 
-                                            let currentMonth = ('0' + (currentDate.getMonth() + 1)).slice(-2);
-                                            let currentDay = ('0' + currentDate.getDate()).slice(-2);
-                                            let currentYear = currentDate.getFullYear();
-                                            let compareDate = `${currentDay}-${currentMonth}-${currentYear}`
+                                            <TableCell className={"header-date"} colSpan={3} align={"center"}>
+                                                {item.date}
+                                            </TableCell>
 
-                                            let slot = '';
-                                            if (item.date === compareDate) {
-                                                if (item1.includes("AM")) {
-                                                    slot = item1.substring(0, 2);
+                                        </TableRow>
 
-
+                                    </TableHead>
+                                    <TableBody>
+                                        <TableRow>
+                                            {item.timeSlots.map((item1, id) => {
+                                                let availabilityClass = '';
+                                                let timeSlotExpired = ''
+                                                if (item1.indexOf(":booked") !== -1) {
+                                                    item1 = item1.split(":")[0];
+                                                    availabilityClass = "booked-time-slot"
                                                 } else {
+                                                    availabilityClass = "available-time-slot"
+                                                }
 
-                                                    let singleTimeDigit = item1.substring(0, 1);
+                                                //let currentDate = this.state.currentDate;
+                                                let currentTime = parseFloat(`${currentDate.getHours()}.${currentDate.getMinutes()}`);
 
-                                                    if (singleTimeDigit === '2') {
-                                                        slot = 14;
-                                                    } else if (singleTimeDigit === '6') {
-                                                        slot = 18;
+                                                let currentMonth = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+                                                let currentDay = ('0' + currentDate.getDate()).slice(-2);
+                                                let currentYear = currentDate.getFullYear();
+                                                let compareDate = `${currentDay}-${currentMonth}-${currentYear}`
+
+                                                let slot = '';
+                                                if (item.date === compareDate) {
+                                                    if (item1.includes("AM")) {
+                                                        slot = item1.substring(0, 2);
+
+
+                                                    } else {
+
+                                                        let singleTimeDigit = item1.substring(0, 1);
+
+                                                        if (singleTimeDigit === '2') {
+                                                            slot = 14;
+                                                        } else if (singleTimeDigit === '6') {
+                                                            slot = 18;
+                                                        }
+                                                    }
+                                                    if (currentTime > parseFloat(slot)) {
+                                                        timeSlotExpired = "expired-time-slot"
                                                     }
                                                 }
-                                                if (currentTime > parseFloat(slot)) {
-                                                    timeSlotExpired = "expired-time-slot"
+                                                //item1 = item1.split(":")[0];
+
+
+                                                if (timeSlotExpired === "expired-time-slot") {
+                                                    return (
+                                                        <TableCell className={`date-timeslot ${timeSlotExpired}`} data-date={item.date} data-timeslot={item1} align={"center"}>
+                                                            {item1}
+                                                        </TableCell>
+                                                    )
+                                                } else if (availabilityClass === "booked-time-slot") {
+                                                    return (
+                                                        <TableCell className={`date-timeslot ${timeSlotExpired} ${availabilityClass}`} data-date={item.date} data-timeslot={item1} align={"center"} >
+                                                            {item1}
+                                                        </TableCell>
+                                                    )
+                                                } else {
+                                                    return (
+                                                        <TableCell className={`date-timeslot ${timeSlotExpired} ${availabilityClass}`} data-date={item.date} data-timeslot={item1} align={"center"} onClick={this.selectTimeSlot}>
+                                                            {item1}
+                                                            <input className="book-timeslot" name="time-slot" type="checkbox" id={item.date + ':' + item1} data-date={item.date} data-timeslot={item1} onClick={this.selectTimeSlot} />
+                                                        </TableCell>
+                                                    )
                                                 }
-                                            }
-                                            //item1 = item1.split(":")[0];
-
-
-                                            if (timeSlotExpired === "expired-time-slot") {
-                                                return (
-                                                    <TableCell className={`date-timeslot ${timeSlotExpired}`} data-date={item.date} data-timeslot={item1} align={"center"}>
-                                                        {item1}
-                                                    </TableCell>
-                                                )
-                                            } else if (availabilityClass === "booked-time-slot") {
-                                                return (
-                                                    <TableCell className={`date-timeslot ${timeSlotExpired} ${availabilityClass}`} data-date={item.date} data-timeslot={item1} align={"center"} >
-                                                        {item1}
-                                                    </TableCell>
-                                                )
-                                            } else {
-                                                return (
-                                                    <TableCell className={`date-timeslot ${timeSlotExpired} ${availabilityClass}`} data-date={item.date} data-timeslot={item1} align={"center"} onClick={this.selectTimeSlot}>
-                                                        {item1}
-                                                        <input className="book-timeslot" name="time-slot" type="checkbox" id={item.date + ':' + item1} data-date={item.date} data-timeslot={item1} onClick={this.selectTimeSlot} />
-                                                    </TableCell>
-                                                )
-                                            }
 
 
 
-                                        })}
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </Paper>
-                    </div>
-                })
-                }
+                                            })}
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </Paper>
+                        </div>
+                    })
+                    }
 
-                <style>{
+                    <style>{
 
-                    `
+                        `
                     .header-date{font-weight: bold;font-size:18px;}
                     .date-timeslot{border-right: 1px solid white;color:white;font-weight:bold;text-align:center;}
                     p.carousel-status, .control-dots{display:none;}
@@ -257,11 +354,11 @@ class CustomTimeSlots extends Component {
                     thead.timeslot-table-head tr th{text-align: center;}
                     .carousel.carousel-slider ul li.slide{border: none !important;}
                     `
-                }
+                    }
 
-                </style>
-            </Carousel >
-
+                    </style>
+                </Carousel >
+            </div >
         );
     }
 }
